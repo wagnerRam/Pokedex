74af91e27e48d90360497221c7ed034fc3c95e5b
Wagner Ram <wagner19ramos@hotmail.com>
2024-06-03 22:22:05 -0300

    Integrando com a PokeApi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('Sucesso aqui')
+
+const offset = 0
+const limit = 13
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}limit=${limit}`
+
+// Quango o fetch der certo chame essa função para interpretar a resposta 
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody)) // sempre o que vai para o segundo then é o retorno do primeiro 
+    .catch((error) => console.error(error))// quando der errado
+
+
+    // independente do resultado
+    // .finally(function () {
+    //     console.log('Requisição concluida')
+    // })
