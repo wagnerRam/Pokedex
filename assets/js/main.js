2200612ed3a3fe4f05add9c51200d4e63517ae40
Wagner Ram <wagner19ramos@hotmail.com>
2024-06-11 20:11:05 -0300

    manipulando o resultado da requisicao, atraves de uma promise

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,17 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?$offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function(response){
-  console.log(response)
-})
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
 
-const x = 10 + 10
-console.log(x)
+  .then((pokemonList) => {
+    console.log(pokemonList)
+  })
+
+  .catch((error) =>  console.error(error))
+  
+
+
+
+  
