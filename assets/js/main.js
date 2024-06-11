0f3c119dd03eb916cee09a71aa1d04eb790b1c24
Wagner Ram <wagner19ramos@hotmail.com>
2024-06-11 19:37:17 -0300

    Fazendo a primeira requisição HTTP com JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,11 @@
+const offset = 0;
+const limit = 10;
 
-const offset = 0
-const limit = 13
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}limit=${limit}`
+const url = `https://pokeapi.co/api/v2/pokemon?$offset=${offset}&limit=${limit}`;
 
-// Quango o fetch der certo chame essa função para interpretar a resposta 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody)) // sempre o que vai para o segundo then é o retorno do primeiro 
-    .catch((error) => console.error(error))// quando der errado
+fetch(url).then(function(response){
+  console.log(response)
+})
 
-
-    // independente do resultado
-    // .finally(function () {
-    //     console.log('Requisição concluida')
-    // })
+const x = 10 + 10
+console.log(x)
