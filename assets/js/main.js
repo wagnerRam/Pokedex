55f00ab43a71329b075f1f1a93c67eab082b03e0
Wagner Ram <wagner19ramos@hotmail.com>
2024-06-11 21:54:27 -0300

    Utilizando a funcao Map, para converter string para HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,36 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?$offset=${offset}&limit=${limit}`;
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
+function convertPokemonToHtml(pokemon) {
+  return `      
 
-  .then((pokemonList) => {
-    console.log(pokemonList)
+    <li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokemon.name}</span>
+
+      <div class="detail">
+        <ol class="types">
+          <li class="type">grass</li>
+          <li class="type">poison</li>
+        </ol>
+
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="${pokemon.name}"/>
+      </div>
+    </li>`;
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi.getpokemons().then((pokemons = []) =>{
+  const newList = pokemons.map((pokemon) => {
+    return convertPokemonToHtml(pokemon)
   })
 
-  .catch((error) =>  console.error(error))
-  
+  const newHtml = newList.join('')
+  pokemonList.innerHTML += newHtml
 
+  console.log(newList)
 
-
-  
+})
